52d78fdd21c803ab652d5e5b635c81348243bcf7
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2026-09-24 22:16:32 +0530

    Update snake_game.java

--- snake_game.java
+++ snake_game.java
@@ -13,56 +13,62 @@ class SnakeGame {
         c = h;
         board = new int[r][c];
         snake = new ArrayDeque<>();
+
+        // Initial snake position
+        snake.addFirst(new int[]{0, 0});
+        board[0][0] = 2;
     }
 
-    private boolean validateNewPosition(int i, int j) {
-        if (i < 0 || j < 0 || i >= r || j >= c) return false;
-        return board[i][j] != 2;
+    private boolean isValidPosition(int i, int j) {
+        return i >= 0 && j >= 0 && i < r && j < c;
     }
 
     public void placeFood(int i, int j) {
-        if (!validateNewPosition(i, j)) return;
-        board[i][j] = 1;
+        if (isValidPosition(i, j) && board[i][j] == 0) {
+            board[i][j] = 1;
+        }
     }
 
     private int[] getNewPosition(String command, int i, int j) {
         if (Objects.equals(command, "U")) {
-            return new int[]{i - 1, j};
+            i--;
         } else if (Objects.equals(command, "R")) {
-            return new int[]{i, j + 1};
+            j++;
         } else if (Objects.equals(command, "D")) {
-            return new int[]{i + 1, j};
+            i++;
         } else if (Objects.equals(command, "L")) {
-            return new int[]{i, j - 1};
+            j--;
         }
+
         return new int[]{i, j};
     }
 
     public int move(String command) {
-        if (snake.isEmpty()) return -1;
-
         int[] currHead = snake.getFirst();
         int[] newHead = getNewPosition(command, currHead[0], currHead[1]);
 
-        int[] tail = null;
-        boolean isFood = (newHead[0] >= 0 && newHead[0] < r && newHead[1] >= 0 && newHead[1] < c && board[newHead[0]][newHead[1]] == 1);
-
-        if (!isFood && !snake.isEmpty()) {
-            tail = snake.pollLast();
-            board[tail[0]][tail[1]] = 0;
-        }
-
-        if (!validateNewPosition(newHead[0], newHead[1])) {
+        // Wall collision
+        if (!isValidPosition(newHead[0], newHead[1])) {
             return -1;
         }
 
-        if (isFood) {
-            board[newHead[0]][newHead[1]] = 2;
-            snake.addFirst(new int[]{newHead[0], newHead[1]});
-        } else {
-            board[newHead[0]][newHead[1]] = 2;
-            snake.addFirst(new int[]{newHead[0], newHead[1]});
+        boolean isFood = board[newHead[0]][newHead[1]] == 1;
+
+        // Remove tail first if we are not eating.
+        // This allows the head to move into the old tail position.
+        if (!isFood) {
+            int[] tail = snake.pollLast();
+            board[tail[0]][tail[1]] = 0;
         }
+
+        // Snake body collision
+        if (board[newHead[0]][newHead[1]] == 2) {
+            return -1;
+        }
+
+        // Add new head
+        snake.addFirst(newHead);
+        board[newHead[0]][newHead[1]] = 2;
 
         return snake.size();
     }
